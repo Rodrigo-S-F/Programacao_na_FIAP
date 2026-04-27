9b2dab5643ac8d03db422ce381a54e00274c2286
labsfiap <rodrigo.rsf420@gmail.com>
2026-04-27 20:58:36 -0300

    aula 5

---
+++ Aula 5.py
@@ -0,0 +1,34 @@
+# soma = 0
+# cont = 0
+# while True:
+#     try:
+#         num = int(input("entre com um nº inteiro"
+#                         " ou 0 para sair = "))
+#
+#         if num == 0:
+#             break
+#         soma += num
+#         cont += 1
+#     except Exception:
+#         print("valor invalido")
+# print(f"soma = {soma} e quantidade de numeros {cont} e média {soma/cont}")
+#
+from types import prepare_class
+
+while True:
+    cod = int(input("qual o codigo do item (0 para exibir o total)? "))
+    if cod == 0
+    elif cod == 1:
+        preço = 0.5
+    elif cod == 2:
+        preço = 1
+    elif cod == 3:
+        preço = 4
+    elif cod == 5:
+        preço = 7
+    elif cod == 9:
+        preço = 8
+    else:
+        print("codigo invalido. ")
+    int(input("quantos? "))
+
