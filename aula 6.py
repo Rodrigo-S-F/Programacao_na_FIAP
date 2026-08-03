1d2792bda76b30262c3bdbd6d4c27a72a3d9b355
labsfiap <RODRIGO.RSF420@GMAIL.COM>
2026-08-03 20:59:35 -0300

    AULA 6

---
+++ aula 6.py
@@ -0,0 +1,22 @@
+# L = [7, 9, 10, 12]
+# num = int(input("Digite um número a pesquisar na lista: "))
+# for i in L:
+#     if i == num:
+#         print("Elemento encontrado!")
+#         break
+# else:
+#  print("Elemento não encontrado.")
+
+# 1. Altere o programa do exemplo anterior de formar a
+# imprimir o menor elemento da lista.
+# L = [1, 7, 2, 4]
+# minimo = L[0]
+# for i in L:
+#     if i < minimo:
+#         minimo = i
+# print(minimo)
+
+# 2. A lista de temperaturas de Mons, na Bélgica, foi
+# armazenada na lista T = [-10, -8, 0, 1, 2, 5, -2, -4]. Faça
+# um programa que imprima a menor e a maior
+# temperatura, assim como a temperatura média.
